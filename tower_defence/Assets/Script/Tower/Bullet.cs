80046c4be7e8d992eb5e5d9ec2bad71ecc718ee9
ksy7461 <sask89@naver.com>
2022-10-07 14:40:40 +0900

    타워 - 주석처리

--- tower_defence/Assets/Script/Tower/Bullet.cs
+++ tower_defence/Assets/Script/Tower/Bullet.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
 
-    float speed = 10.0f;
+    float speed = 10.0f; // 총알 스피드
     Rigidbody2D rb;
 
     private void Awake()
@@ -15,15 +15,14 @@ public class Bullet : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //rb.AddForce(speed * Time.deltaTime * Vector3.forward,ForceMode2D.Impulse);
-        rb.velocity = transform.right * speed;
+        rb.velocity = transform.right * speed; // 총알에게 velocity값을 준다.(일정하게 움직이기 위해)
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Enemy"))
         {
-            Destroy(this.gameObject);
+            Destroy(this.gameObject); // 적을 만나면 총알을 제거
         }
     }
 }
--- tower_defence/Assets/Script/Tower/Tower.cs
+++ tower_defence/Assets/Script/Tower/Tower.cs
@@ -5,41 +5,44 @@ using static UnityEngine.GraphicsBuffer;
 
 public class Tower : MonoBehaviour
 {
-    public GameObject bullet;
-    public float attackSpeed = 1.0f;
+    public GameObject bullet; // 공격 투사체
 
-    private List<GameObject> Enemys;
-    Transform target = null;
+    public float attackSpeed = 1.0f; // 타워 공격 주기
+    float attackDamage; // 타워 공격력
+    bool isphysics = false; // 물리/마법 타워 구별
+
+    private List<GameObject> Enemys; // 적 List로 받기
+    Transform target = null; // 공격 대상
 
 
     private void Awake()
     {
-        Enemys = new List<GameObject>();
+        Enemys = new List<GameObject>(); // 적 리스트 할당
     }
 
     private void Start()
     {
-        StartCoroutine(PeriodAttack());
+        StartCoroutine(PeriodAttack()); // 공격 코루틴 시작
     }
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Enemy"))
+        if (collision.CompareTag("Enemy")) // 적을 만날때
         {
-            Enemys.Add(collision.gameObject);
+            Enemys.Add(collision.gameObject); // 적을 리스트에 추가
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Enemy"))
+        if (collision.CompareTag("Enemy")) //적이 나갈때
         {
             foreach (GameObject enemy in Enemys)
             {
-                if (enemy == collision.gameObject)
+                if (enemy == collision.gameObject) // 나간 적이 리스트에 있다면
                 {
-                    Enemys.Remove(enemy);
+                    Enemys.Remove(enemy); //그 적을 리스트에 제거
                     break;
                 }
             }
@@ -48,22 +51,22 @@ public class Tower : MonoBehaviour
 
     private void Attack()
     {
-        target = Enemys[0].transform;
-        Vector3 targetDir = (target.position - transform.position).normalized;
-        float angle = Vector2.SignedAngle(Vector2.right, targetDir);
+        target = Enemys[0].transform; //리스트의 첫번째 적에게 공격
+        Vector3 targetDir = (target.position - transform.position).normalized; // 방향을 구한후
+        float angle = Vector2.SignedAngle(Vector2.right, targetDir); // 각도 구하기
 
-        Instantiate(bullet, transform.position, Quaternion.Euler(new Vector3(0, 0, angle)));
+        Instantiate(bullet, transform.position, Quaternion.Euler(new Vector3(0, 0, angle))); // 총알을 적의 방향으로 생성
     }
 
     IEnumerator PeriodAttack()
     {
         while (true)
         {
-            if (Enemys.Count != 0)
+            if (Enemys.Count != 0) //적이 있다면
             {
-                Attack();
+                Attack(); //공격을 시작
             }
-            yield return new WaitForSeconds(attackSpeed);
+            yield return new WaitForSeconds(attackSpeed); // 공격주기
         }
     }
 }
