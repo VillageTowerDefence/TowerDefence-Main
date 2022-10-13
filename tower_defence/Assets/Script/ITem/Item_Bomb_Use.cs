77e79a4db890dc0b62c1680d85c316db164a1b06
Hyeong1204 <cho4870@naver.com>
2022-10-13 16:26:32 +0900

    폭탄, 불 아이템 에셋 추가

--- tower_defence/Assets/Script/ITem/Item_Bomb_Use.cs
+++ tower_defence/Assets/Script/ITem/Item_Bomb_Use.cs
@@ -7,21 +7,36 @@ using UnityEngine;
 public class Item_Bomb_Use : MonoBehaviour
 {
     public Action onItemUse;        // 델리게이트
+    Transform explostion;
+    SpriteRenderer sprite;
 
+    private void Awake()
+    {
+        explostion = transform.GetChild(0);
+        sprite = GetComponent<SpriteRenderer>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("ObstacleTile"))   // 장애물과 만나면
         {
-            StartCoroutine(Distroy());          // 코루틴 시작
+            StartCoroutine(ExplosionStart());          // 코루틴 시작
         }
+    }
+
+    IEnumerator ExplosionStart()
+    {
+        yield return new WaitForSeconds(4.0f);
+        StartCoroutine(Distroy());
+        sprite.color = Color.clear;
+        explostion.gameObject.SetActive(true);
+        onItemUse?.Invoke();                // Tile_Obstacle로 델리게이트 보냄
     }
 
     IEnumerator Distroy()
     {
-        yield return new WaitForSeconds(2.0f);      // 2초 후에 (라운드 시스템 들어가면 라운드마다 터지게 바꾸기)
+        yield return new WaitForSeconds(1.0f);      // 2초 후에 (라운드 시스템 들어가면 라운드마다 터지게 바꾸기)
 
-        onItemUse?.Invoke();                // Tile_Obstacle로 델리게이트 보냄
         Destroy(this.gameObject);           // 자기 자신 삭제하기
     }
 }
