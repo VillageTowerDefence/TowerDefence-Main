5c9a76ddbca262857eab4cf277e394206d5e27c9
Hyeong1204 <cho4870@naver.com>
2022-11-09 14:18:20 +0900

    아이템 데이터 주석추가

--- tower_defence/Assets/Script/ITem/ScriptableObject/ItemBuffData.cs
+++ tower_defence/Assets/Script/ITem/ScriptableObject/ItemBuffData.cs
@@ -7,9 +7,9 @@ public class ItemBuffData : ScriptableObject
 {
     public uint id = 0;                     // 아이템 ID
     public string itemName = "아이템";      // 아이템 이름
-    public GameObject modelprefab;         // 아이템의 외형을 표시할 프리펩
+    public GameObject modelprefab;         // 아이템 모델 프리펩
     [Header("아이템 가격")]
-    public uint value;                     // 아이템의 가격
+    public uint itemCost;                  // 아이템의 가격
     [Header("아이템 수량")]
-    public uint Count = 0;                 // 소유중인 아이템 갯수
+    public uint count = 0;                 // 소유중인 아이템 갯수
 }
