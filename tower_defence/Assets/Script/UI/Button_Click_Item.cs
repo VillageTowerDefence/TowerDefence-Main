9efa36efe2ab0071b152b1cbe420ad7c9e6f2b8a
HyoungJiKim <khj90305@gmail.com>
2022-11-21 14:26:09 +0900

    UI - 상점

--- tower_defence/Assets/Script/UI/Button_Click_Item.cs
+++ tower_defence/Assets/Script/UI/Button_Click_Item.cs
@@ -1,15 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Button_Click_Item : MonoBehaviour
 {
     public GameObject buy_panel;
 
+    public ItemData itemData;
+
+    public ItemData ItemData
+    {
+        get => itemData;
+    }
+
+    Image itemImage;
+    TextMeshProUGUI itemPrice;
+
+    private void Awake()
+    {
+        itemImage = transform.GetChild(0).GetComponent<Image>();
+        itemPrice = transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+    }
+    private void Start()
+    {
+        if (ItemData == null)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            transform.name = ItemData.name;
+            itemImage.sprite = ItemData.modelprefab.GetComponent<SpriteRenderer>().sprite;
+            itemPrice.text = ItemData.itemCost.ToString();
+        }
+    }
+
     public void Open_Buy_Panel()
     {
-        GameObject gameObject = transform.gameObject;
-        buy_panel.GetComponent<Button_Buy_Item>().buy_item = gameObject;
         buy_panel.SetActive(true);
+        buy_panel.GetComponent<Button_Buy_Item>().Display_ShopWindow(ItemData);
     }
 }
